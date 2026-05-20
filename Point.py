b47e1838bd6dbecc5345cfe6aa33c4ba452b67ed
andriii-droid <andri.andermatt@icloud.com>
2026-05-20 10:08:43 +0200

    Point Doc

--- Point.py
+++ Point.py
@@ -2,6 +2,7 @@ import math
 
 class Point:
     def __init__(self, x=0.0, y=0.0):
+        '''Creates a Point Instance using cartesian coordinates'''
         # We establish Cartesian as our single internal source of truth
         self._x = float(x)
         self._y = float(y)
@@ -16,20 +17,21 @@ class Point:
         return cls(x, y) # Calls __init__ with the calculated x and y
     
     def __add__(self, other):
-        """Allows: new_point = p1 + p2"""
+        """Implements Addition of two points"""
         if not isinstance(other, Point):
             return NotImplemented
         # Vector addition: add X components together, and Y components together
         return Point(x=self._x + other._x, y=self._y + other._y)
 
     def __sub__(self, other):
-        """Allows: new_point = p1 - p2"""
+        """Implements Subtraction of two Points"""
         if not isinstance(other, Point):
             return NotImplemented
         # Vector subtraction: subtract other components from self
         return Point(x=self._x - other._x, y=self._y - other._y)
     
     def distance(self, other):
+        '''calculates distance between two point objects'''
         if not isinstance(other, Point):
             return NotImplemented
         return ((self._x - other._x)**2 + (self._y - other._y)**2)**0.5
@@ -37,10 +39,12 @@ class Point:
     # --- Getters (Properties) ---
     @property
     def cartesian(self):
+        '''returns the cartesian coordinates as a tuple'''
         return (round(self._x, 10), round(self._y, 10))
 
     @property
     def polar(self):
+        '''returns the polar coordinates as a tuple'''
         angle = math.degrees(math.atan2(self._y, self._x)) % 360
         distance = math.hypot(self._x, self._y) # Cleaner way to do (x^2 + y^2)^0.5
         return (round(angle, 10), round(distance, 10))
