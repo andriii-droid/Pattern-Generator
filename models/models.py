e9cd6377c138130d162ac9689b19490afb218858
andriii-droid <andri.andermatt@icloud.com>
2026-05-27 11:05:19 +0200

    defined Datamodels for datatransfer

---
+++ models/models.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+from point import Point
+
+@dataclass
+class PatternConfig:
+    '''Data contract for global pattern settings'''
+    num_center_points: int
+    center_point_radius: float
+
+@dataclass
+class FileConfig:
+    '''Data contract for Filesettings'''
+    filename: str
+    gcode_offset_x: float
+    gcode_offset_y: float
+
+@dataclass
+class DrawingConfig:
+    '''Data contract for drawing settings'''
+    draw_points: bool
+    draw_lines: bool
+    draw_sketch: bool
+    draw_coordinates: bool
+
+
+@dataclass
+class ShapeConfig:
+    """Data contract for shapes"""
+    shape_type: int
+    num_shapes: int
+    size: float
+    hex_color: str
+    offset: float
+    line_points: int
+
+@dataclass
+class SplineConfig:
+    """Data contract for complex spline tracks."""
+    show_spline: bool
+    num_points: int
+    start_point: Point
+    control_point: Point
+    end_point: Point
+
