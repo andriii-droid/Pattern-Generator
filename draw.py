2af5cc81d13000d955ddda8d9481ff9a1a149087
andriii-droid <andri.andermatt@icloud.com>
2026-05-29 10:14:43 +0200

    scale points

--- draw.py
+++ draw.py
@@ -18,9 +18,9 @@ class Draw():
     def draw_points(self, pat: Shape | Spline):
         content = ""
         for p in pat.points:
-            x = p.x + self._center_point.x
-            y = p.y + self._center_point.y
-            content += f'''<circle cx="{x*self._scale_factor}" cy="{y*self._scale_factor}" r="1" fill="black" />'''
+            x = (p.x + self._center_point.x) * self._scale_factor
+            y = (p.y + self._center_point.y) * self._scale_factor
+            content += f'''<circle cx="{x}" cy="{y}" r="1" fill="black" />'''
         return content
     
     def draw_shape_lines(self, shape: Shape):
@@ -28,17 +28,17 @@ class Draw():
         point_lists = [shape.points[i:i + shape.config.shape_type] for i in range(0, len(shape.points), shape.config.shape_type)]
         for point_shape in point_lists:
             for count, (p1, p2) in enumerate(zip(point_shape, point_shape[1:]+[point_shape[0]])):
-                p1 += self._center_point
-                p2 += self._center_point
-                content += f'''<line x1="{p1.x*self._scale_factor}" y1="{p1.y*self._scale_factor}" 
-                x2="{p2.x*self._scale_factor}" y2="{p2.y*self._scale_factor}" fill="none" stroke="{shape.config.hex_color}" stroke-width=".2" />'''
+                p1 = (p1 + self._center_point) * self._scale_factor
+                p2 = (p2 + self._center_point)  * self._scale_factor
+                content += f'''<line x1="{p1.x}" y1="{p1.y}" 
+                x2="{p2.x}" y2="{p2.y}" fill="none" stroke="{shape.config.hex_color}" stroke-width=".2" />'''
         return content
     
     def draw_lines_between_line_points(self, shape: Shape):
         content = ""
         for count, (p1, p2) in enumerate(zip(shape.points, shape.points[shape.config.line_points+2:]+shape.points[0:shape.config.line_points+2])):
-            p1 += self._center_point
-            p2 += self._center_point
-            content += f'''<line x1="{p1.x*self._scale_factor}" y1="{p1.y*self._scale_factor}" 
-            x2="{p2.x*self._scale_factor}" y2="{p2.y*self._scale_factor}" fill="none" stroke="{shape.config.hex_color}" stroke-width=".2" />'''
+            p1 = (p1 + self._center_point) * self._scale_factor
+            p2 = (p2 + self._center_point)  * self._scale_factor
+            content += f'''<line x1="{p1.x}" y1="{p1.y}" 
+            x2="{p2.x}" y2="{p2.y}" fill="none" stroke="{shape.config.hex_color}" stroke-width=".2" />'''
         return content
