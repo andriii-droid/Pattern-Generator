310e03c555fd46d33d9164e661e3dce51b3f461a
andriii-droid <andri.andermatt@icloud.com>
2026-06-03 16:19:22 +0200

    chip colors

--- ui/components/line_selector.py
+++ ui/components/line_selector.py
@@ -5,18 +5,25 @@ class LineSelector():
         self.chips_list = []
 
         with ui.row().classes('items-center') as self.chips:
-            self.label_input = ui.select(with_input=True,options=[]).on('keydown.enter', self.add_line_chip)
+            self.label_input = ui.select(with_input=True,options=[]).on('keydown.enter', self.add_line_chip).classes('w-50')
             with self.label_input.add_slot('append'):
                 ui.button(icon='add', on_click=self.add_line_chip).props('round dense flat')
 
     def add_line_chip(self):
-        if len(self.chips_list) != 2:
+        if len(self.chips_list) == 2:
+            ui.notify('Only two patterns can be defined!', type='warning')
+        elif self.label_input.value is None:
+            ui.notify('Choose a pattern from the dropdown', type='warning')
+        elif self.label_input.value.startswith("Spline"):
             with self.chips:
-                chip = ui.chip(self.label_input.value, icon='label', color='silver', removable=True)
+                chip = ui.chip(self.label_input.value, icon='label', color='green', removable=True)
                 chip.on('remove', lambda: self.handle_chip_removal(chip))
-                self.chips_list.append(chip)
+                self.chips_list.append(chip)   
         else:
-            ui.notify('only two patterns can be defined!', type='warning')
+            with self.chips:
+                chip = ui.chip(self.label_input.value, icon='label', color='blue', removable=True)
+                chip.on('remove', lambda: self.handle_chip_removal(chip))
+                self.chips_list.append(chip)        
         self.label_input.value = ''
 
     def handle_chip_removal(self, chip_instance):
